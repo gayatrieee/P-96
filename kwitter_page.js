d0f5ba041c6983e55811d566131b1730a95fb0e3
gayatrieee <88025844+gayatrieee@users.noreply.github.com>
2022-06-20 15:19:38 +0800

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,20 @@
+function logout() {
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+     window.location = "kwitter.html";
+     }
+     function send()
+        {
+            msg= document.getElementById("msg").value;
+            firebase.database().ref(room_name).push({
+                name:user_name
+                message:msg,
+                like:0
+            });
+            document.getElementById("msg").value= "";
+          }
+          firebase.database().ref(room_name).push({
+              name:user_name,
+              message:msg,
+              like:0
+          });
